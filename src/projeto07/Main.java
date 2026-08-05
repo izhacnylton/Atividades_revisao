d03ec17d6bb37a1cc0c017b1a3dcf9199da4beb7
Izhac Nylton <izhac2006@gmail.com>
2026-08-05 15:00:19 -0400

    Implement Main class to calculate rectangle properties

--- src/projeto07/Main.java
+++ src/projeto07/Main.java
@@ -1,1 +1,16 @@
+package projeto07;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Retangulo r1 = new Retangulo(5.0, 10.0);
+		
+		System.out.printf("Base:  %.2f", r1.base);
+		System.out.printf("\nAltura: %.2f", r1.altura);
+		
+		System.out.printf("\nCálculo da área: %.2f", r1.calcularArea());
+		
+		System.out.printf("\nCálculo do perímetro: %.2f", r1.calcularPerimetro());
+		
+	}
+}
