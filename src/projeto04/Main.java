0b2ce66373b4b8b98712a3f4a02d37b7f1e8c79e
Izhac Nylton <izhac2006@gmail.com>
2026-08-04 22:47:54 -0400

    Add Main class with student average calculation

---
+++ src/projeto04/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno("Marquinho", 5, 9);
+	
+		System.out.println("Media: " + a1.calcularMedia());
+		System.out.println("Estado: ");
+		a1.verificarSituacao();	
+		
+	}
+}
