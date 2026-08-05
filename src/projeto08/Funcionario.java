aadfba8e1e19ded610dc958d08e1dc1fbc9afed9
Izhac Nylton <izhac2006@gmail.com>
2026-08-05 16:24:58 -0400

    Add Funcionario class with salary methods

--- src/projeto08/Funcionario.java
+++ src/projeto08/Funcionario.java
@@ -1,1 +1,23 @@
+package projeto08;
 
+public class Funcionario {
+	String nome, cargo;
+	double salario;
+	
+	public Funcionario(String nome, String cargo, double salario){
+		this.nome = nome;
+		this.cargo = cargo;
+		this.salario = salario;
+		}
+		
+	public double aumentarSalario(double percentual){
+			return salario = salario + (salario * percentual);
+		}
+		
+	public void exibirDados(){
+		System.out.println("Nome: " + nome);
+		System.out.println("Cargo: " + cargo);
+		System.out.printf("Salário: R$ %.2f", salario);
+		}
+	
+}
