54ddf0441b4493a3d1981e8ca5c80d510bcadf09
Izhac Nylton <izhac2006@gmail.com>
2026-08-05 17:12:59 -0400

    Implement temperature conversion in Main class

--- src/projeto09/Main.java
+++ src/projeto09/Main.java
@@ -1,1 +1,12 @@
+package projeto09;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		ConversorTemperatura ct1 = new ConversorTemperatura();
+		
+		System.out.printf("\nConvertendo 35°C para Fahrenheit: %.2f", ct1.converterCelsius(35));
+		System.out.printf("\nConvertendo 78 F° para Celsius: %.2f", ct1.converterFahrenheit (78));
+		
+	}
+}
