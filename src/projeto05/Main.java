b9edc1c9ea93d149b1f0fd2b04b1c4afc164ffc4
Izhac Nylton <izhac2006@gmail.com>
2026-08-04 23:54:34 -0400

    Implement product management in Main class

--- src/projeto05/Main.java
+++ src/projeto05/Main.java
@@ -1,1 +1,29 @@
+package Projeto05;
 
+public class Main {
+	public static void main(String[] args) {
+		Produto p1 = new Produto("Bergamota", 4.44, 144);
+		
+		System.out.println("Nome: " + p1.nome);
+		System.out.println("Estoque atual: " + p1.estoque);
+		System.out.println("Preco individual do produto: R$ " +  p1.preco);
+		System.out.println("Valor total do estoque: " + p1.calcularTotal());
+		
+		
+		//adicionando
+		
+		p1.adicionar(50);
+		
+		System.out.println("Novo estoque: " + p1.estoque);
+		System.out.println("Novo Valor total do estoque: " + p1.calcularTotal());
+
+    //removendo lroduto
+    
+		p1.remover(30);
+		
+		System.out.println("Novo estoque: " + p1.estoque);
+		System.out.println("Novo Valor total do estoque: " + p1.calcularTotal());
+		
+		
+	}
+}
