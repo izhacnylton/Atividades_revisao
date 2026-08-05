59dbb6cff9ed780332b63c93b8e203c5ee0fcd41
Izhac Nylton <izhac2006@gmail.com>
2026-08-05 16:24:17 -0400

    Implement main method to manage Funcionario
    Added a main method to create a Funcionario object, display its data, and increase its salary by 10%.

--- src/projeto08/Main.java
+++ src/projeto08/Main.java
@@ -1,1 +1,15 @@
+package projeto08;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Funcionario f1 = new Funcionario("Marquinho", "Aviaozinho do tráfico",  1500.00);
+		
+		f1.exibirDados();
+		
+		f1.aumentarSalario(0.10);
+		System.out.println("\nSalário aumentado em 10%\n");
+		
+		f1.exibirDados();
+	}
+}
