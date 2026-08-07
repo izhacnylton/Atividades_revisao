211a562feeeb9dc242d245e3a7f6f344f77f313f
Izhac Nylton <izhac2006@gmail.com>
2026-08-07 16:31:11 -0400

    Rename Main class to Atividade04

--- src/atividadeArrays/Atividade04/Atividade04.java
+++ src/atividadeArrays/Atividade04/Atividade04.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class Atividade04 {
     public static void main(String[] args) {
         Map<String, Double> produtos = new HashMap<>();
 
