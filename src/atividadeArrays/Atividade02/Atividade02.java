b8f05ca7dbaf5684538c09844df3f4cfbc5df56d
Izhac Nylton <izhac2006@gmail.com>
2026-08-07 16:29:04 -0400

    Rename Main class to Atividade02

--- src/atividadeArrays/Atividade02/Atividade02.java
+++ src/atividadeArrays/Atividade02/Atividade02.java
@@ -1,6 +1,6 @@
 import java.util.LinkedList;
 
-public class Main {
+public class Atividade02 {
     public static void main(String[] args) {
         
         LinkedList<String> filaImpressao = new LinkedList<>();
