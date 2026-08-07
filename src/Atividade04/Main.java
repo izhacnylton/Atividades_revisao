69e9d43f79a268af1321b9fe7eb92cc581ef3375
Izhac Nylton <izhac2006@gmail.com>
2026-08-07 16:08:13 -0400

    Implement product price retrieval using HashMap
    Added a Java program that uses a HashMap to store product prices and retrieves the price of a specific product.

---
+++ src/Atividade04/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Double> produtos = new HashMap<>();
+
+        produtos.put("Teclado", 150.0);
+        produtos.put("Mouse", 80.0);
+        produtos.put("Monitor", 900.0);
+
+        produtos.put("Mouse", 95.50);
+
+        String produtoBuscado = "Mouse";
+        Double preco = produtos.get(produtoBuscado);
+
+        System.out.println("O preço atual do " + produtoBuscado + " é: R$ " + preco);
+    }
+}
